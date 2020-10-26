8830518999a0bbdc914fe7031ec0b636c44af7ce
Pranay Kapoor <pranaykapoor@atlassian.com>
2020-10-27 02:02:32 +0530

    first commit

---
+++ MyApplication_login/app/src/main/java/com/example/myapplication_login/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication_login.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
